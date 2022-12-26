c637ac83a8a6c79a432667da104f5686f655febb
Ehsan-Fouladi <fooehan3@gmail.com>
2022-12-26 23:17:50 +0330

    update

--- home/views.py
+++ home/views.py
@@ -3,3 +3,9 @@ from django.views.generic.base import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'home/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        self.request.session.get("my_name", "HACKER")
+        print(self.request.session.get("my_name", "HACKER"))
+        return context
