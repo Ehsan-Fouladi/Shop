85f67554a129ddbe0319212af8e3ef1deb047d98
Ehsan-Fouladi <fooehan3@gmail.com>
2023-01-09 21:25:30 +0330

    update cart add

--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 app_name = "cart"
 urlpatterns = [
-    path("detail/", views.CartDetailView.as_view(), name="cart_detail")
+    path("detail/", views.CartDetailView.as_view(), name="cart_detail"),
+    path("add/", views.CartAddView.as_view(), name="cart_add"),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 
 
@@ -6,3 +6,10 @@ from django.views import View
 class CartDetailView(View):
     def get(self, request):
         return render(request, "cart/cart_detail.html", {})
+
+
+
+class CartAddView(View):
+    def post(self, request):
+        # print("product added to cart")
+        return redirect("cart:cart_detail")
